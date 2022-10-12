eb9615332dfd35dd186fc71a5b13e7dc669f332e
Om Patel <opatel37@yahoo.com>
2022-10-12 09:58:54 -0600

    bubble sort init

---
+++ CS30-Bubble-Sort-Assignment/main.py
@@ -0,0 +1,12 @@
+
+def swap(list, idx1, idx2):
+
+    list[idx1], list[idx2] = list[idx2], list[idx1]
+
+
+
+def bubbleSort(anArray): 
+    for total_comps in range(len(anArray) - 1, 0, -1):
+        for cur_coms in total_comps:
+                    
+
