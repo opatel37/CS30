7eb0aea8706497a6e2f60a16b751325e11f9e695
Om Patel <opatel37@yahoo.com>
2022-10-14 09:15:45 -0600

    help update

--- CS30-Bubble-Sort-Assignment/main.py
+++ CS30-Bubble-Sort-Assignment/main.py
@@ -1,6 +1,6 @@
 
 nums = [10, 70, 30, 100, 40, 45, 90, 80, 85]
-words = ["dog","at", "good", "eye", "cat", "ball", "fish"]
+words = ["dog", "at", "good", "eye", "cat", "ball", "fish"]
 
 def swap(list, idx1, idx2):
 
@@ -10,15 +10,19 @@ def swap(list, idx1, idx2):
 
 def bubbleSort(anArray):
 
-    for total_comps in range(len(anArray) - 1, 0, -1):
-        idx1 = 0
-        idx2 = idx1 + 1
+    for total_coms in range(len(anArray)-1, -1, -1):
 
-        for cur_coms in total_comps:
-            if anArray[idx1] > anArray[idx2]:
-                swap(anArray, idx1, idx2)
+        print(anArray[total_coms])
 
-            cur_coms -= 1
+        # idx1 = 0
+        # idx2 = idx1 + 1
+
+        # for cur_coms in range(total_coms):
+        #     if anArray[idx1] > anArray[idx2]:
+        #         swap(anArray, idx1, idx2)
+
+        #     idx1 += 1
+        #     cur_coms -= 1
 
 
 test1 = bubbleSort(nums)
