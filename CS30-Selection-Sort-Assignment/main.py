1cc499a6dc0d550ae9de761db0b2f1022fbc18fb
Om Patel <opatel37@yahoo.com>
2022-10-18 09:23:03 -0600

    selection sort final update

--- CS30-Selection-Sort-Assignment/main.py
+++ CS30-Selection-Sort-Assignment/main.py
@@ -2,10 +2,26 @@ nums = [10, 70, 30, 100, 40, 45, 90, 80, 85]
 words = ["dog","at", "good", "eye", "cat", "ball", "fish"]
  
 def selectionSort(anArray):
-    for fill_slot in range(0, len(anArray) -2, 1):
+    # Loop through array, stopping one before the last element
+    for fill_slot in range(0, len(anArray) -1):
+        # Create and set the fill slot(what might be swapped)
         min_position = fill_slot
-        for post_fill in range(anArray[fill_slot] +1, len(anArray) -2, 1):
-            if post_fill < anArray[min_position]:
+
+        # Loop through values after the fill slot element
+        for post_fill in range(fill_slot +1, len(anArray)):
+            
+            # Check if items after fill slot are smaller
+            if anArray[post_fill] < anArray[min_position]:
+                # If so, set min position to the new minimum value
                 min_position = post_fill
 
-            list[min_position], list[fill_slot] = list[fill_slot], list[min_position]
+        # Swap values at min position and fill slot (will only make a difference if you have set min position to new value[post_fill])
+        anArray[min_position], anArray[fill_slot] = anArray[fill_slot], anArray[min_position]
+
+# Sort array(s)
+selectionSort(nums)
+selectionSort(words)
+
+# Print array(s)
+print(nums)
+print(words)
