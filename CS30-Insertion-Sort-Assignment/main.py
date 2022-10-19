2abb4f32e8dd5ba76d541462f8a9d03de1e38d1b
Om Patel <opatel37@yahoo.com>
2022-10-19 09:44:05 -0600

    finished insertion sort

--- CS30-Insertion-Sort-Assignment/main.py
+++ CS30-Insertion-Sort-Assignment/main.py
@@ -3,8 +3,36 @@ words = ["dog","at", "good", "eye", "cat", "ball", "fish"]
 
 def insertionSort(anArray):
     for i in range(1, len(anArray) -1):
-        ins_value = anArray[i]
-        ins_position = i
+        ins_val = anArray[i]
+        test_pos = i
 
-    while ins_position > 0 and ins_value > anArray[ins_position -1]:
+        while test_pos > 0 and  anArray[test_pos -1] > ins_val:
+            anArray[test_pos] = anArray[test_pos -1]
+            test_pos -= 1
         
+        anArray[test_pos] = ins_val
+
+insertionSort(nums)
+insertionSort(words)
+
+print(nums)
+print(words)
+
+
+
+# ins_val = 40
+# test_pos = 4
+
+# 4 > 0
+# 40 < 100
+
+# anArray[test_pos] (40) = anArray[test_pos -1] (100)
+# [10, 70, 30, 100, 100, 45, 90, 80, 85]
+
+# test_pos = 3
+
+# 3 > 0
+# 30 > 40 (terminates while loop)
+
+# anArray[test_pos -1] (100) = ins_val (what's been saved as 40)
+
