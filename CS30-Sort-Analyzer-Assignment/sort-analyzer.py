761ec5c63645b85d0794152ab23da273053f1329
UKFGL <96450681+UKFGL@users.noreply.github.com>
2022-10-20 08:47:23 -0600

    Add files via upload

---
+++ CS30-Sort-Analyzer-Assignment/sort-analyzer.py
@@ -0,0 +1,37 @@
+# SORT ANALYZER STARTER CODE
+
+import time
+
+# RETURN DATA FROM FILE AS AN ARRAY OF INTERGERS
+def loadDataArray(fileName):
+    temp = []
+
+    # Read file line by line
+    fileref = open(fileName, "r")
+    for line in fileref:
+        line = line.strip()  # Clean up line
+        temp.append(int(line))  # Add integer to temp list
+
+    fileref.close()
+
+    return temp
+
+
+# LOAD DATA FILE INTO GLOBAL VARIABLES
+randomData = loadDataArray("data-files/random-values.txt")
+reversedData = loadDataArray("data-files/reversed-values.txt")
+nearlySortedData = loadDataArray("data-files/nearly-sorted-values.txt")
+fewUniqueData = loadDataArray("data-files/few-unique-values.txt")
+
+# VERIFY LOADED DATA BY PRINTING FIRST 50 ELEMENTS
+print(randomData[0:50])
+print(reversedData[0:50])
+print(nearlySortedData[0:50])
+print(fewUniqueData[0:50])
+
+
+# EXAMPLE OF HOW TO TIME DURATION OF A SORT ALGORITHM
+# startTime = time.time()
+# bubbleSort(randomData)
+# endTime = time.time()
+# print(f"Bubble Sort Random Data: {endTime - startTime} seconds")
