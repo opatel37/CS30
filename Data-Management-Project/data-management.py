59fc5ce342de6185afd5ee3144da5327fc67f669
Om Patel <opatel37@yahoo.com>
2022-11-04 08:52:35 -0600

    emergency update

--- Data-Management-Project/data-management.py
+++ Data-Management-Project/data-management.py
@@ -1,16 +1,20 @@
-from xml.etree.ElementTree import TreeBuilder
-import functions
-import json 
-
+# import functions
+import json
+import pandas
 # Data Management Project
 
 # Global Variable(s)
-loop = True
 
-# Main Menu (while loop)
-while loop:
-    print(
-'''
+
+def main():
+
+    # Local Variable(s)
+    loop = True
+
+    # Main Menu (while loop)
+    while loop:
+        print(
+        '''
 DATE MANAGEMENT MAIN MENU
 
 1. Display All Data
@@ -19,6 +23,30 @@ DATE MANAGEMENT MAIN MENU
 4. Add to Favourites List
 5. Remove Data from Favourites List
 6. Display Favourites List
+7. Exit
 
-'''
-    )
+        '''
+            )
+
+        selection = input("Input number of desired option (1-7): ")
+
+        match selection:
+            case "1":
+                test = pandas.read_excel(r"F:\CS30\Data-Management-Project\trade-data-set.csv")
+                print(test)
+            case "2":
+                print("2")
+            case "3":
+                print("3")
+            case "4":
+                print("4")
+            case "5":
+                print("5")
+            case "6":
+                print("6")
+            case "7":
+                loop = False
+                print ("Program Terminated")
+
+
+main()
