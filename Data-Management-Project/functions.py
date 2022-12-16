363dd6fd8c83013f3ab9276f72fdbd812a42500f
Om Patel <opatel37@yahoo.com>
2022-12-16 09:56:06 -0700

    updates

--- Data-Management-Project/functions.py
+++ Data-Management-Project/functions.py
@@ -33,9 +33,9 @@ def sort_dec(val_1, val_2):
         return True
 
 def search_data(list, key, val):
-  for item in list:
-      if item[key] == val:
-          return list.index(item)
+  for i in range(len(list)):
+      if list[i][key] == val:
+          return i
 
   return -1
 
@@ -94,10 +94,17 @@ def print_10_lines(list, line_num):
         print("\n" + str(list[i]))
 
 def filter_search(list, filter, key):
+    temp_list = []
     for item in list:
         if item[key] == filter.capitalize():
-            print("\n" + str(item))
-
+            temp_list.append(item)
+    
+    if temp_list == []:
+        print("No trades took place that day, please try again")
+        return temp_list
+    else:
+        return temp_list
+    
 def add_to_fav(list, date, username):
     for i in range(0, len(list)):
         if list[i]["Date"] == date:
@@ -111,3 +118,185 @@ def add_cur_user_fav(list, user):
         if list[i]["Username"] == user["Username"]:
             list[i]["Favorites"] = user["Favorites"]
  
+def print_out_list(list):
+   for i in range(len(list)):
+        print("\n" + str(list[i]))
+
+def main_menu(data, user_creds, all_users):
+    # Main Menu (while loop)
+    while main_loop:
+        print(
+        '''
+DATE MANAGEMENT MAIN MENU
+
+1. Display First 10 Items in Data Set
+2. Search/Filter Data
+3. Sort Data
+4. Add to Favourites List
+5. Remove Data from Favourites List
+6. Display Favourites List
+7. Exit
+
+        '''
+            )
+
+        select = input("Input number of desired option (1-7): ")
+
+        match select:
+            case "1":
+
+                # Track lines printed
+                line_num_1 = 0
+
+                # print(data_list[line_num_1:line_num_1 + 10])
+                for i in range(line_num_1, line_num_1 + 10):
+                    print("\n" + str(data[i]))
+                
+                # Set inner loop
+                inner_loop_1 = True
+
+                while inner_loop_1:
+                    print(
+'''
+1. Display the next 10 items
+2. Track back to last set of items displayed on previous visit
+3. Return to Main Menu
+'''
+                    )
+
+                    # Get input for inner loop
+                    select_1 = input("Input number of desired option (1-3): ")
+
+                    # Process input
+                    if select_1 == "1":
+                        # Add to line tracker
+                        line_num_1 += 10
+
+                        # print(data_list[line_num_1:line_num_1 + 10])
+                        print_10_lines(data, line_num_1) 
+                    
+                    elif select_1 == "2":
+                        # Set current line # to the one the user was at during last session
+                        line_num_1 = line_num_1_archive
+
+                        # print(data_list[line_num_1:line_num_1 + 10])
+                        print_10_lines(data, line_num_1)
+
+                    elif select_1 == "3":
+                        line_num_1_archive = line_num_1
+                        inner_loop_1 = False
+
+                    else:
+                        print("Invalid Entry")
+
+            case "2":
+                # Set inner loop 
+                inner_loop_2 = True
+
+                while inner_loop_2:
+                    print(
+'''
+Filter By: 
+1. Day of the Week
+2. Date
+3. Return to Main Menu
+'''
+                    )
+
+                    select_2 = input("Input number of desired option (1-3): ")
+
+                    if select_2 == "1":
+                        day = input("Enter the day you would like to see data for: ")
+
+                        filtered_search = filter_search(data, day, "Weekday")
+
+                        print_out_list(filtered_search)
+
+                    elif select_2 == "2":
+                        date = input("Enter the date you would like to see data for (YYYY-MM-DD): ")
+
+                        filtered_search = filter_search(data, date, "Date")
+
+                        print_out_list(filtered_search)
+
+                    elif select_2 == "3":
+                        inner_loop_2 = False
+                    
+                    else: 
+                        print("Invalid Entry")
+            
+            case "3":
+                # Set inner loop 
+                inner_loop_3 = True
+
+                while inner_loop_3:
+                    print(
+'''
+Sort By:
+1. Date
+2. Cumulative Value (Increasing)
+3. Cumulative Value (Decreasing)
+4. Return to Main Menu
+'''
+                    )
+
+                    select_3 = input("Input number of desired option (1-4): ")
+
+
+                    match select_3:
+                        case "1":
+                            selection_sort(data, "Date", sort_inc)
+                            print(data)
+
+                        case "2":
+                            selection_sort(data, "Cumulative", sort_inc)
+                            print(data)
+
+
+                        case "3":
+                            selection_sort(data, "Cumulative", sort_dec)
+                            print(data)
+                
+                        case "4":
+                            inner_loop_3 = False
+                    
+                        case other:
+                            print("Invalid Entry")
+
+            case "4":
+                select_4 = input("Input date of trade you wish to add to favorites (YYYY-MM-DD): ")
+
+                # Functionize so if no trade took place that day it returns -1 and check if trade is already in fav list
+                for i in range(0, len(data)):
+                    if data[i]["Date"] == select_4:
+                        user_creds["Favorites"].append(data[i])
+                
+                # Add to current user's fav list
+                add_cur_user_fav(all_users, user_creds["Favorites"])               
+                write_data(all_users, './text-files/users.txt')
+            
+            case "5":
+                select_5 = input("Input date of trade you wish to remove from favorites (YYYY-MM-DD): ")
+
+                search_return_val = search_data(user_creds, "Date", select_5)
+                
+                if search_return_val == -1:
+                    print("Data was not found in list")
+                else:
+                    user_creds.pop(search_return_val)
+                    # Add to current user's fav list
+                    add_cur_user_fav(all_users, user_creds)
+                    
+                write_data(all_users, './text-files/favorites.txt')
+
+            case "6":
+                for i in range(0, len(user_creds["Favorites"])):
+                    print("\n" + str(user_creds["Favorites"][i]))
+
+
+            case "7":
+                main_loop = False
+                print ("Signed Out")
+
+            case other:
+                print("Invalid Entry")
